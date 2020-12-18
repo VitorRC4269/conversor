c1db4bd83e5600fe2d021a9f9b8a27cd44df7cb8
VitorRC4269 <vitorramosdecarvalho@gmail.com>
2020-12-18 01:43:33 -0300

    commit teste 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,11 @@ import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=ed0e5683";
-void main() async{
-
- 
-
-  runApp(MaterialApp(
-    home: Container()
-  ));
+void main() async {
+  runApp(MaterialApp(home: Home()));
 }
 
-Future<Map> getData() async{
+Future<Map> getData() async {
   http.Response response = await http.get(request);
   return json.decode(response.body);
 }
@@ -33,6 +28,36 @@ class _HomeState extends State<Home> {
         title: Text("\$ Conversor \$"),
         backgroundColor: Colors.amber,
         centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text(
+                    "Erro ao Carregar Dados :(",
+                    style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(
+                  color: Colors.green,
+                );
+              }
+          }
+        },
       ),
     );
   }
